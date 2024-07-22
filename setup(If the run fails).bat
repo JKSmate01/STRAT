282fa145dc2e87d7bd13abd52f1e4bf6951b473e
JKSmate01 <93654099+JKSmate01@users.noreply.github.com>
2024-07-22 14:44:26 +0200

    Delete setup(If the run fails).bat

--- setup(If the run fails).bat
+++
@@ -1,9 +0,0 @@
-@echo off
-SET SCRIPT_DIR=%~dp0
-cd /d %SCRIPT_DIR%
-python -m venv venv
-call venv\Scripts\activate
-pip install -r requirements.txt
-deactivate
-echo Virtual environment setup is complete and requirements are installed.
-pause
